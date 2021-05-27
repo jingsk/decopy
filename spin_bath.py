e6836b2a1ce325f2d6f26d13f8aa8cb8ef16f079
Jing Trerayapiwat <jingsk129@gmail.com>
2021-05-27 14:56:59 -0400

    added docstring to spin_bath.py

--- spin_bath.py
+++ spin_bath.py
@@ -3,7 +3,11 @@ import numpy as np
 from qutip import basis
 
 class spin_bath(bath):
-
+   """
+   makes bath hamiltonian based on cce order
+   should be created after knowing cce order 
+   (also for converging cce_order)
+   """
    def __init__(self, gyromagneticratio_data_set)
    self.bath_sites=bath
    self.gyro=gyro_data_set
@@ -15,14 +19,19 @@ class spin_bath(bath):
       self.hamiltonian=make_hamiltonian(bath_sites.get_positions(), cce_order)
 
 def generate_initial_state(n_sites):
+   """
+   randomly assign up or down magnetic state
+   """
    state_list=[]
    for site in range(n_site):
-      #randomly assign up or down magnetic state
       state=basis(2, intnp.rand())
       state_list.append(state)
    return np.array(state_list)
 
 def make_hamiltonian(positions, cce_order):
+   """
+   for cce=2, for each site, loop thru other sites and make h_nn
+   """
    h_list=[]
    if cce_order ==2:
       h_list=[]
@@ -32,6 +41,9 @@ def make_hamiltonian(positions, cce_order):
       return np.array(h_list)
 
 def get_nn_interactions(site1, site2, gyro):
+   """
+   calculate h_nn based on eq in paper 
+   """
    r1=site1.positions
    r2=site2.positions
    I1=state_vector(site1.index)
